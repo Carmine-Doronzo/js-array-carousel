a395688abb3c3df8e118e4c830587d377b6c5c2d
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-08 17:11:21 +0100

    ADD event listener

--- js/script.js
+++ js/script.js
@@ -3,18 +3,88 @@ const arrayPathImg = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/
 
 // verifico  che l'array si legga in console
 //console.log(arrayPathImg);
-
+let i = 0;
 //dichiaro la costante containerDomElement per utilizzarla nel ciclo e fuori dal ciclo
-const containerDomElement = document.querySelector('.container')
+const containerDomElement = document.querySelector('.container');
+
 //creo un ciclo for per scorrere l'array e inserire contenuto nel container elemento di html
-for(let i = 0; i < arrayPathImg.length; i++){
-    console.log(arrayPathImg[i])
+for(i = 0; i < arrayPathImg.length; i++){
+    console.log(arrayPathImg[i]);
     //stampo nel dom il codice contenente '<div class="item"><img src="./img/.webp" alt=""></div>' sostituendo il percorso con il contenuto dell'array
     
-    containerDomElement.innerHTML += `
+    if(i == 0){
+     containerDomElement.innerHTML += `
     <div class="item active">
         <img src="${arrayPathImg[i]}" alt="">
     </div>`;
+    }else{
+        containerDomElement.innerHTML += `
+    <div class="item">
+        <img src="${arrayPathImg[i]}" alt="">
+    </div>`;
+
+    }
+
+
 }
+
+const nextDomElement = document.querySelector('.next');
+
+const prevDomElement = document.querySelector('.prev');
+
+const activeDomClass = document.querySelectorAll('.active');
+
+const itemDomElements = document.querySelectorAll('.item'); 
+
+i = 0
+nextDomElement.addEventListener('click',function(){
+
+    
+    i++    
+    
+    activeDomClass[i].classList.remove('active')
+
+    itemDomElements[i].classList.add('active')
+
+    
+
+
+
+    console.log(i)
+    
+
+    
+    
+    
+    
+    
+
+    
+
+
+
+    
+
+
+})
+
+
+
+prevDomElement.addEventListener('click', function(){
+
+    i--    
+    
+    activeDomClass[i].classList.remove('active')
+
+    itemDomElements[i].classList.add('active')
+
+    
+
+
+
+    console.log(i)
+    
+
+})
 
 
